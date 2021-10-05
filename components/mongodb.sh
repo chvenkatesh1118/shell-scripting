6cd643527a65b5d6e1726269f8af3a465f1f61d4
EC2 Default User <ec2-user@ip-172-31-31-98.ec2.internal>
2021-10-05 11:09:37 +0000

    2

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -6,3 +6,19 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+
+ yum install -y mongodb-org 
+ # systemctl enable mongod
+ # systemctl start mongod
+ 
+# sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+ 
+ 
+#sudo systemctl restart mongod
+ 
+  #curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+# cd /tmp
+# unzip mongodb.zip
+ #cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js 
