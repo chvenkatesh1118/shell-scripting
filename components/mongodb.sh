a8cc58668a345c095100b47c219acfcc279a81d7
chvenkatesh1118 <91310867+chvenkatesh1118@users.noreply.github.com>
2021-10-12 12:42:55 +0530

    11

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -5,20 +5,33 @@ name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+statuscheck $?
 
- yum install -y mongodb-org 
- # systemctl enable mongod
- # systemctl start mongod
+#gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+
+# yum install -y mongodb-org 
+ #statuscheck $?
+# systemctl enable mongod
+# statuscheck $?
+# systemctl start mongod
+# statuscheck $?
  
-# sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+#sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+statuscheck $?
  
  
 #sudo systemctl restart mongod
+ statuscheck $?
  
   #curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-# cd /tmp
-# unzip mongodb.zip
+  statuscheck $?
+# cd /tmp 
+statuscheck $?
+# unzip mongodb.zip 
+statuscheck $?
  #cd mongodb-main
+ statuscheck $?
 # mongo < catalogue.js
+statuscheck $?
 # mongo < users.js 
+statuscheck $?
